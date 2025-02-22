a375e5c73a77fba70efe33e35a20bf65af0ea9a4
Jakub Bukowski <littlefellapp@gmail.com>
2025-02-22 17:31:11 +0100

    correct answer task #5

--- #5.cpp
+++ #5.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
 
+long div(long init = 20) {
+    while(true) {
+        bool is_found = true;
+        for(short i = 1; i < 21; i++) {
+            if(init % i != 0) {
+                is_found = false;
+                break;
+            }
+        }
+        if(is_found) {
+            return init;
+        }
+        init++;
+    }
+}
+
 int main() {
-    cout << "kociak";
+    cout << div();
     return 0;
 }
